a22c3e1f6d5f0db7a9d69199a4bef7dea84d4a65
Julia Kartasheva <kartjulia@gmail.com>
2021-03-21 19:09:25 -0700

    adding about link

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function myFunction1() {
-    document.getElementById("Q1").innerHTML = "Go online to our Eventbrite page. Registration is important to raise funding for supplies. You may also donate supplies and/or funds. Check out our links below.";
+function signUp() {
+    var txt;
+    var person = prompt("Please enter your name:", "Harry Potter");
+    if (person == null || person == "") {
+      txt = "You are not signed up.";
+    } else {
+      txt = "Thank you " + person + "! You have successfully signed up!";
+    }
+    document.getElementById("message").innerHTML = txt;
 }
 
-function myFunction2() {
-    document.getElementById("Q2").innerHTML = "Please bring personal protective equipment (PPE): face masks ect. We will supply latex gloves for trash pick up. We will also provide garbage bags. At lunch, we will provide a small snack too.";
+function visitPage(){
+    window.location='https://www.facebook.com/empoderedu/';
+
+    /* 
+    How to do this in JQuery:
+    <button id="some_id">Visit Page Now</button>
+
+    $('#some_id').click(function() {
+        window.location='http://www.example.com';
+    });
+    */
 }
+
+
 
 function myFunction3() {
     document.getElementById("Q3").innerHTML = "Please meet next to the white canopy, set up on the grass directly behind the basketball courts.";
@@ -18,14 +36,5 @@ function myFunction5() {
     document.getElementById("Q5").innerHTML = "Please fill out the following Google Form if you wish to donate items. Please scan our Venmo QR code to donate funds.";
 }
 
-function myFunction() {
-    var txt;
-    var person = prompt("Please enter your name:", "Harry Potter");
-    if (person == null || person == "") {
-      txt = "You are not signed up.";
-    } else {
-      txt = "Thank you " + person + "! You have successfully signed up!";
-    }
-    document.getElementById("demo").innerHTML = txt;
-}
+
 
